b5611f4650798eb97ea132af444dc238943e8d85
mohamed-behery <mohamedbehery.coding@gmail.com>
2023-12-29 16:02:57 +0200

    Added The Rest of Post Controllers

--- server/controllers/PostControllers.js
+++ server/controllers/PostControllers.js
@@ -34,4 +34,53 @@ const updatePost = async (req, res) => {
   }
 };
 
-export { createNewPost, updatePost };
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.deleteOne();
+        res.status(200).json("Post has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export { createNewPost, updatePost, deletePost, getPost, getAllPosts };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,11 @@
 import express from "express";
-import Post from "../models/Post.js";
-import { createNewPost, updatePost } from "../controllers/PostControllers.js";
+import {
+  createNewPost,
+  updatePost,
+  deletePost,
+  getPost,
+  getAllPosts,
+} from "../controllers/PostControllers.js";
 
 const router = express.Router();
 
@@ -11,55 +16,12 @@ router.post("/", createNewPost);
 router.put("/:id", updatePost);
 
 //DELETE POST
-router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        await post.deleteOne();
-        res.status(200).json("Post has been deleted");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can delete only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", deletePost);
 
 //GET POST
-router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/:id", getPost);
 
 //GET ALL POSTS
-router.get("/", async (req, res) => {
-  const username = req.query.user;
-  const catName = req.query.cat;
-  try {
-    let posts;
-    if (username) {
-      posts = await Post.find({ username });
-    } else if (catName) {
-      posts = await Post.find({
-        categories: {
-          $in: [catName],
-        },
-      });
-    } else {
-      posts = await Post.find();
-    }
-    res.status(200).json(posts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/", getAllPosts);
 
 export default router;
