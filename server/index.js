e7c52d4b6fcdecd7a3328d7bd8ea62ecb3a70844
mohamed-behery <mohamedbehery.coding@gmail.com>
2023-12-30 13:20:54 +0200

    Removed The Temp Images on The Backend

--- server/index.js
+++ server/index.js
@@ -3,12 +3,10 @@ const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoute from "./routes/auth.js";
-import userRoute from "./routes/users.js";
-import postRoute from "./routes/posts.js";
-import categoryRoute from "./routes/categories.js";
-import multer from "multer";
-import path from "path";
+import authRoute from "./routes/Auth.js";
+import userRoute from "./routes/Users.js";
+import postRoute from "./routes/Posts.js";
+import categoryRoute from "./routes/Categories.js";
 
 app.use(express.json());
 app.use(cors());
@@ -19,21 +17,6 @@ function connect() {
   console.log("Connected to DB");
 }
 connect();
-
-// multer is a library for basic image upload, but we can use firebase or aws in big projects
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
