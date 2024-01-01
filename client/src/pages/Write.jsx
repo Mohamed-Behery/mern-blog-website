91379a4284712ecd9b33e87e548b4ab72934e5bc
mohamed-behery <mohamedbehery.coding@gmail.com>
2024-01-01 14:38:09 +0200

    Added The Strucutre of The Write Page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
 const Container = styled.div``;
+const Image = styled.img``;
+const Title = styled.input``;
+const Category = styled.input``;
+const Content = styled.textarea``;
 
 const Write = () => {
-  return <Container>Write</Container>;
+  return (
+    <Container>
+      <Image src="" />
+      <Title type="text" placeholder="Post Title" />
+      <Category type="text" placeholder="Post Category" />
+      <Content placeholder="Post Content" />
+    </Container>
+  );
 };
 
 export default Write;
